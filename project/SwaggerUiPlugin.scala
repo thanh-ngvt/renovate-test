551db072fdfb054e1a0692b1d79a2703e7e50b9c
null <vtnguyen@samhammer.de>
2025-10-09 08:37:46 +0000

    Update Minor and patch dependency updates

--- project/SwaggerUiPlugin.scala
+++ project/SwaggerUiPlugin.scala
@@ -19,7 +19,7 @@ object SwaggerUiPlugin extends AutoPlugin {
 
   import autoImport.*
 
-  val swaggerUiVersion = "5.28.1"
+  val swaggerUiVersion = "5.29.1"
 
   override lazy val projectSettings = Seq(
     swaggerUiGenerate := { mappings: Seq[PathMapping] =>
